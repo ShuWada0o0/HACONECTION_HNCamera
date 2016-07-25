c441839b377c07bcf20464e27df3039af8bbce7f
永井陽太 <nagaiyouta@nagaiyouta-no-MacBook-Air.local>
2016-07-26 02:11:49 +0900

    スクロール後の画面にボタンを配置し画像を挿入した スクロールはまだできていない

--- app/src/main/java/com/example/hirotoshin/myapplication/ScrollingActivity.java
+++ app/src/main/java/com/example/hirotoshin/myapplication/ScrollingActivity.java
@@ -1,13 +1,24 @@
 package com.example.hirotoshin.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class ScrollingActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+        setView();
+    }
+
+    private void setView(){
+        ImageButton emotional = (ImageButton)findViewById(R.id.stampEmotional);
+        ImageButton physics = (ImageButton)findViewById(R.id.stampphysics);
+        ImageButton calture = (ImageButton)findViewById(R.id.stampcalture);
+        emotional.setImageResource(R.drawable.stampemotional2);
+        physics.setImageResource(R.drawable.stampphysical2);
+        calture.setImageResource(R.drawable.stumpculture2);
     }
 }
